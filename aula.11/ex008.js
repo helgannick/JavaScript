15588240a3c8a6cb097444907ba2e67249a8b591
helgannick <helgannick@gmail.com>
2022-08-21 17:53:22 -0300

    Exercícios JS

---
+++ aula.11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`A veloxidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log(`Você utrapassou a veloxidade permitida. MULTADO!!`);
+
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
