133a2fe9fbc9ba8cf1f1e7215dc200ed58577ba6
helgannick <helgannick@gmail.com>
2022-09-01 19:08:18 -0300

    debug ex 016

--- aula.14ex/ex016/Contar.js
+++ aula.14ex/ex016/Contar.js
@@ -4,24 +4,26 @@ function Contar() {
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossível contar!'
-
-    } else {
+   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] faltam dados')
+   } else { 
         res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+
         if (i < f) {
-            // Contagem crescente
+        //Contagem crescente 
             for (let c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} `
+            res.innerHTML += `${c} \u{1F449}`
+            
             }
         } else {
             //Contagem regressiva
-            for (let c = i; c >= f; c -= p) {
-                res.innerHTML += ` ${c} `
+            for (let c = i; c >= f; c -=p) {
+                res.innerHTML += `${c} \u{1F449}`
             }
         }
-    }
+        res.innerHTML += `\u{1F3C1}`
+   }
 }
