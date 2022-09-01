977a58a35446b3c8675cbd7d52aedd4fcfdacb3d
helgannick <helgannick@gmail.com>
2022-09-01 18:23:54 -0300

    aula14\ex016

---
+++ aula.14ex/ex016/Contar.js
@@ -0,0 +1,27 @@
+function Contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} `
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} `
+            }
+        }
+    }
+}
