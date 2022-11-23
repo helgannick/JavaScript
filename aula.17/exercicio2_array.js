07be076d92f773bab56bfe64c7abdcc318ec2b5d
marcos barbosa <helgannick@gmail.com>
2022-11-23 06:45:13 -0300

    exercicio array

---
+++ aula.17/exercicio2_array.js
@@ -0,0 +1,14 @@
+
+const numeros = [0,1,2,3,4,5,8,10,12,16];
+
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    if (numero % 2 === 1){
+        console.log(numero)
+    }
+
+
+    
+
+}
